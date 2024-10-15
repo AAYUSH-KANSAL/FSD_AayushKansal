8e560834e3c25599844c21ae060697c1dfb1de72
AAYUSH-KANSAL <ayush.kansal321@gmail.com>
2024-10-15 14:28:40 +0530

    arraw function

--- DAY_5/script.js
+++ DAY_5/script.js
@@ -1,12 +1,25 @@
-// // let x=confirm("Are you sure to logout")
-// // alert(x);
-// let v= prompt("Enter your namae: ","Rahul");
-// alert(v);
+// // // // let x=confirm("Are you sure to logout")
+// // // // alert(x);
+// // // let v= prompt("Enter your namae: ","Rahul");
+// // // alert(v);
 
 
 
-let disp=()=>{
-    console.log("welcome to array function in js")
+// // let disp=()=>{
+// //     console.log("welcome to array function in js")
+// // }
+
+// // disp();
+
+// let say=()=> console.log(" single arrow function");
+// say();
+
+
+let sum=(x,y)=>{
+    return x+y;
 }
 
-disp();
+
+let res=sum(12,32);
+console.log(res);
+ 
